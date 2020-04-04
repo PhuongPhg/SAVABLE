0d27f3bec8ab605c3342c8121009122cf72fa41c
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-04-04 22:48:29 +0700

    Update welcome.php and welcome.css

--- save.php
+++ save.php
@@ -7,25 +7,29 @@ $name = "";
 $usernamee = "";
 $email = "";
 $passwordd = "";
+$check=0;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(isset($_POST["name"])) { $name = $_POST['name']; }
     if(isset($_POST["user-name"])) { $usernamee = $_POST['user-name']; }
     if(isset($_POST["email"])) { $email = $_POST['email']; }
     if(isset($_POST["pass-word"])) { $passwordd = $_POST['pass-word']; }
 }
-
-
+    if($name=="" || $usernamee=="" || $email=="" || $passwordd==""){
+      echo "Some information is misssing. Do it again please!";
+    }
+    else {
     //Code xử lý, insert dữ liệu vào table
     $sql = "INSERT INTO UserAccount (name, username, email, password)
     VALUES ('$name', '$usernamee', '$email', '$passwordd')";
 
     if ($connect->query($sql) === TRUE) {
         echo "";
+       $check=1;
     } else {
         echo "Error: " . $sql . "<br>" . $connect->error;
     }
-
+  }
 //Đóng database
 include "disconnection.php";
-require("welcome.php");
+ if ($check==1){ require("welcome.php");}
 ?>
