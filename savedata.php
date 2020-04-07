e9991889f1402376ce03b734e8c42d1481952dfa
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-04-07 23:14:48 +0700

    savedata and indexcreate for index.php

---
+++ savedata.php
@@ -0,0 +1,39 @@
+<?php
+include("connection.php");
+
+session_start();
+$namee="";
+$achieve=0;
+$budget=0;
+$id = 0;
+$update = false;
+$userr=$_SESSION['username'];
+
+
+	if (isset($_POST['save'])) {
+	$namee = $_POST['namee'];
+    $achieve=$_POST['achieve'];
+    $budget=$_POST['budget'];
+
+    mysqli_query($connect,"INSERT INTO wishlist (id, username, name, achieve, budget) VALUES($id, '$userr', '$namee', $achieve, $budget)");
+		header('location: index.php');
+		exit;
+	}
+  if (isset($_POST['update'])) {
+	$id = $_POST['id'];
+	$namee = $_POST['namee'];
+	$achieve = $_POST['achieve'];
+  $budget=$_POST['budget'];
+
+	mysqli_query($connect, "UPDATE wishlist SET name='$namee', achieve=$achieve, budget=$budget WHERE id=$id");
+	header('location: index.php');
+	exit;
+  }
+
+  if (isset($_GET['del'])) {
+	$id = $_GET['del'];
+	mysqli_query($connect, "DELETE FROM wishlist WHERE id=$id");
+	header('location: index.php');
+	exit;
+}
+?>
