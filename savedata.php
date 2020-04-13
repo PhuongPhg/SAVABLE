88f2a80da307a98c790babc28ec2d178c68f09b5
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-04-13 11:20:58 +0700

    split file

--- savedata.php
+++ savedata.php
@@ -30,10 +30,5 @@ $userr=$_SESSION['username'];
 	exit;
   }
 
-  if (isset($_GET['del'])) {
-	$id = $_GET['del'];
-	mysqli_query($connect, "DELETE FROM wishlist WHERE id=$id");
-	header('location: index.php');
-	exit;
 }
 ?>
