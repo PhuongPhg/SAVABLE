bb2f0b02d3264f17dd4d392f921c77fb621f9b5e
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-04-29 08:52:11 +0700

    Update .json files to cypress test

---
+++ cypress test/plugins/index.js
@@ -0,0 +1,47 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+const mysql = require ('mysql')
+function queryTestDb(query, config) {
+  // creates a new mysql connection using credentials from cypress.json env's
+  const connection = mysql.createConnection(config.env.db)
+  // start connection to db
+  connection.connect()
+  // exec query + disconnect to db as a Promise
+  return new Promise((resolve, reject) => {
+   connection.query(query, (error, results) => {
+      if (error) reject(error)
+      else {
+        connection.end()
+        // console.log(results)
+        return resolve(results)
+      }
+    })
+  })
+}
+
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+  on('task', {
+    queryDb: query => {
+      return queryTestDb(query, config)
+    },
+  })
+  
+}
