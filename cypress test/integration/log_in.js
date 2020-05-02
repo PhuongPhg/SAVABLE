e3f363ac91dbd85e69eea5e7ea11b33e4dc9e274
Trinh Thao Phuong <thaophuong.trinh.9@gmail.com>
2020-05-02 11:30:04 +0700

    Test case with log in #34

--- cypress test/integration/log_in.js
+++ cypress test/integration/log_in.js
@@ -1,20 +1,80 @@
-describe('Login page test',  () => {
-    it('Navigate to login page', () => {
-        cy.visit('https://letmetryy.000webhostapp.com/html/login.html');
+describe('The log in page', () => {
+    it('Navigator to log in page', () => {
+        cy.visit('/html/login.html')
     })
+    it('Wrong password', () => {
+        //cy.typeLogIn('fakename', 'FakeName')
 
-    it('Input login info', () =>{
         cy.get("form");
 
-        cy.get('.username')
-          .type('fakename')
-          .should('have.value', 'fakename');
+        cy.get('input[name=user-name]')
+        .clear()
+        .type('fakename')
+        .should('have.value', 'fakename');
 
-        cy.get('.password')
-          .type('FakeName9')
-          .should('have.value', 'FakeName9');
+        cy.get('input[type=password]')
+        .type('FakeName')
+        .should('have.value', 'FakeName');
 
         cy.get("form").submit();
-        cy.contains('Log out').click()
+
+        cy.contains('Wrong password. Check it again! Return')
+        cy.contains('Return').click()
+    })
+
+    it('Wrong username', () => {
+        cy.get("form");
+
+        cy.get('input[name=user-name]')
+        .clear()
+        .type('fakenam')
+        .should('have.value', 'fakenam');
+
+        cy.get('input[type=password]')
+        .clear()
+        .type('FakeName9')
+        .should('have.value', 'FakeName9');
+
+        cy.get("form").submit();
+
+        cy.contains('This username does not exist. Check it again! Return')
+        cy.contains('Return').click()
+    })
+
+    it('Missing password', () => {
+        cy.get("form")
+
+        cy.get('input[name=user-name]')
+        .clear()
+        .type('fakename')
+        .should('have.value', 'fakename');
+
+        cy.get('input[type=password]').clear()
+
+        cy.get("form").submit();
+        cy.contains('Some information is missing. Check it again! Return')
+        cy.contains('Return').click()
+    })
+
+    it('Missing username', () => {
+        cy.get("form")
+
+        cy.get('input[name=user-name]').clear()
+
+        cy.get('input[type=password]')
+        .clear()
+        .type('FakeName9')
+        .should('have.value', 'FakeName9');
+
+        cy.get("form").submit();
+
+        cy.contains('Some information is missing. Check it again! Return')
+        cy.contains('Return').click()
+    })
+
+    it('Valid username / password', () => {
+        //I add this typeLogIn command in /support/commands.js
+        cy.typeLogIn('fakename', 'FakeName9')
+        cy.url().should('include', '/index.php')
     })
 })
--- cypress test/support/commands.js
+++ cypress test/support/commands.js
@@ -23,3 +23,11 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+Cypress.Commands.add('typeLogIn', (username, pw) => {
+    cy.get("form");
+
+    cy.get('input[name=user-name]').clear().type(username)
+    cy.get('input[type=password]').clear().type(pw)
+
+    cy.get("form").submit();
+})
