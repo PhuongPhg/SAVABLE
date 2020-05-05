6a89b7088d4c718931d8fa68f7b9b1e3dc860c54
Phạm Minh Long <parksoojin_sm_kr@yahoo.com.vn>
2020-05-05 21:54:54 +0700

    Sign up page accepts existing email #39

--- php/save.php
+++ php/save.php
@@ -8,6 +8,7 @@ $usernamee = "";
 $email = "";
 $passwordd = "";
 $yearofbirth = "";
+$condition = 0;
 $check=0;
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(isset($_POST["name"])) { $name = $_POST['name']; }
@@ -16,27 +17,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if(isset($_POST["pass-word"])) { $passwordd = $_POST['pass-word']; }
     if(isset($_POST["yob"])) { $yearofbirth = $_POST['yob']; }
 }
-    if($name=="" || $usernamee=="" || $email=="" || $passwordd=="" || $yearofbirth==""){
-      echo "Some information is misssing. Do it again please! <a href='javascript: history.go(-1)'>Return</a>";
-      exit;
-    }
-    else {
-      $sql="SELECT * FROM UserAccount WHERE username='$usernamee'";
-      $kt=mysqli_query($connect, $sql);
+if($name=="" || $usernamee=="" || $email=="" || $passwordd=="" || $yearofbirth==""){
+  echo "Some information is misssing. Do it again please! <a href='javascript: history.go(-1)'>Return</a>";
+  exit;
+}
+else {
+  $sql="SELECT * FROM UserAccount WHERE username='$usernamee'";
+  $kt=mysqli_query($connect, $sql);
 
-      if(mysqli_num_rows($kt)>0) {
-        echo "This email username already exist! <a href='javascript: history.go(-1)'>Return</a>";
-      }
-      else {
+  if(mysqli_num_rows($kt)>0) {
+    echo "This username has already existed!";
+  }
+  else{
+    $condition++;
+  }
+      
+  $sql="SELECT * FROM UserAccount WHERE email='$email'";
+  $kt=mysqli_query($connect, $sql);
+
+  if(mysqli_num_rows($kt)>0) {
+    echo "This email has already existed!";
+  }
+  else{
+    $condition++;
+  }
+  if($condition == 2){
     //Code xử lý, insert dữ liệu vào table
     $sql = "INSERT INTO UserAccount (name, username, email, password, yob)
     VALUES ('$name', '$usernamee', '$email', '$passwordd', '$yearofbirth')";
 
-    if ($connect->query($sql) === TRUE) {
-        echo "";
-       $check=1;
-    } else {
-        echo "Error: " . $sql . "<br>" . $connect->error;
+    if ($connect->query($sql) == TRUE) {
+      echo "";
+      $check=1;
+    } 
+    else {
+      echo "Error: " . $sql . "<br>" . $connect->error;
     }
   }
 }
